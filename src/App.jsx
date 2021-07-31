918b8d5be71018f8715b6f2081e476a4e233c370
tran duc huy <huysusu125@gmail.com>
2021-07-31 15:28:31 +0700

    v1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import Header from './components/Header'
+import Todos from './components/Todos';
+const App = () => {
+    return (
+        <Fragment>
+            <Header />
+            <Todos />
+        </Fragment>
+    )
+}
+
+export default App;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Todos = () => {
+    return (
+        <div>
+            <p>viec 1</p>
+            <p>viec 3</p>
+            <p>viec 2</p>
+        </div>
+    )
+}
+
+export default Todos;
